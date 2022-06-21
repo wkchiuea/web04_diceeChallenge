c265c9e465356b4c1283fae550d443cde68be123
wkchiuea <103587188+wkchiuea@users.noreply.github.com>
2022-06-21 15:41:42 +0800

    [update] step4-5

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
+const IMGPATH = "images/dice";
+
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector("img1").setAttribute("src", `${IMGPATH}${randomNumber1}.png`);
+document.querySelector("img2").setAttribute("src", `${IMGPATH}${randomNumber2}.png`);
+
