4d120e2c9138e897b0647e51a3f2c3b018a53652
wkchiuea <103587188+wkchiuea@users.noreply.github.com>
2022-06-21 15:57:24 +0800

    [update] display winner

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 document.querySelector(".img1").setAttribute("src", `${IMGPATH}${randomNumber1}.png`);
 document.querySelector(".img2").setAttribute("src", `${IMGPATH}${randomNumber2}.png`);
 
+var finalMsg = "";
+if (randomNumber1 == randomNumber2) {
+    finalMsg = "Draw!";
+} else {
+    finalMsg = `Player ${(randomNumber1 > randomNumber2) ? randomNumber1 : randomNumber2} wins!`
+}
+
+document.querySelector("h1").innerHTML = finalMsg;
