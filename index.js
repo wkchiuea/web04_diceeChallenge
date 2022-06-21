c1665ab93eb4cf249ee7ac2536da3594ed300bc9
wkchiuea <103587188+wkchiuea@users.noreply.github.com>
2022-06-21 15:45:14 +0800

    [fix] minor fix

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const IMGPATH = "images/dice";
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector("img1").setAttribute("src", `${IMGPATH}${randomNumber1}.png`);
-document.querySelector("img2").setAttribute("src", `${IMGPATH}${randomNumber2}.png`);
+document.querySelector(".img1").setAttribute("src", `${IMGPATH}${randomNumber1}.png`);
+document.querySelector(".img2").setAttribute("src", `${IMGPATH}${randomNumber2}.png`);
 
